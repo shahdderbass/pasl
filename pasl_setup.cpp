e0a27f8e863ec59c21adbe46e3ca10add0fa12ff
shahdderbass <s2231172085@ku.edu.kw>
2025-12-17 13:18:51 +0300

    Update pasl_setup.cpp

--- pasl_setup.cpp
+++ pasl_setup.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 // include PASL headers as provided in your course setup
-// e.g., #include "pasl/pasl.hpp" (use the exact include from your lecture setup)
+#include "pasl/pasl.hpp"
 
 int main() {
   std::cout << "PASL setup OK\n";
